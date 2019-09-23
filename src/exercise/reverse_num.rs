b4ac0aea97e3f559a9adaadf8fbc6814a9404723
gordon <a731450465@qq.com>
2019-09-23 23:48:32 +0800

    整数翻转

--- src/exercise/reverse_num.rs
+++ src/exercise/reverse_num.rs
@@ -1,6 +1,39 @@
-use std::i32;
 
-fn reverse_num(num:i32) -> i32 {
+///
+/// 翻转数字
+///
+/// # Example
+///
+/// ```test
+///
+/// assert_eq!(-321, reverse(-123));
+/// assert_eq!(0, reverse(2147483647));
+///
+/// ```
+///
+pub fn reverse(x: i32) -> i32 {
 
-    
+    use std::i32;
+    let mut x = x;
+    let mut result:Option<i32> = Some(0);
+
+    loop {
+        if x == 0 {
+            break;
+        }
+        let tmp_num = x % 10;
+        x = x / 10;
+
+        result = result.unwrap().checked_mul(10);
+        if result.is_none() {
+            return 0i32;
+        }
+
+        result = tmp_num.checked_add(result.unwrap());
+        if result.is_none() {
+            return 0_i32;
+        }
+    }
+
+    result.unwrap()
 }
